4694445d13e7094dc5d8015972a7624759165dab
Trond Graabræk <43748103+trondag@users.noreply.github.com>
2020-02-15 14:23:29 +0100

    Add project files.

---
+++ WebRender/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WebRender
+{
+    public interface Tag
+    {
+        String getText();
+    }
+}
---
+++ WebRender/Class3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebRender
+{
+    class ParagraphTag : Tag
+    {
+        private String text;
+
+        public ParagraphTag(String text) => this.text = text;
+
+        public string getText() => this.text;
+    }
+}
